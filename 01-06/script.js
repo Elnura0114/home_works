3eb0aa684b33097b8582026caf0a2ca28fa05882
Алчагул <elnura.doktur@gmail.com>
2022-06-02 18:17:53 +0600

    feat: add home work 01/06/2022

---
+++ 01-06/script.js
@@ -0,0 +1,22 @@
+const userSalaries = {
+    Ella : 35000,
+    Sophia : 45000,
+    Ellie : 30000
+}
+console.log(userSalaries.Ella + userSalaries.Sophia);
+
+const key = {
+    a : 5,
+    b : 6,
+    c : 10
+}
+console.log(key.a + key.b + key.c);
+
+let number;
+if(data === true){
+    number = 3;
+} else{
+    number = 5;
+}
+
+const result = data === true ? 3 : 5;
