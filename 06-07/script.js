59d779492c0c75a5ed00d1f1454030ef8ba18dc1
Алчагул <elnura.doktur@gmail.com>
2022-07-06 22:11:39 +0600

    feat: added H/W from 05/07

---
+++ 06-07/script.js
@@ -0,0 +1,27 @@
+/*
+const red_2 = document.querySelector('#red_color');
+const blue_2 = document.querySelector('#blue_color');
+const purple_2 = document.querySelector('#purple_color');
+const container_2 = document.querySelector('#color_container');
+
+red_2.addEventListener('click', () => {
+    container_2.classList.toggle('background-red');
+})
+
+blue_2.addEventListener('click', () => {
+    container_2.classList.toggle('background-blue');
+})
+
+purple_2.addEventListener('click', () => {
+    container_2.classList.toggle('background-purple');
+})*/
+
+
+
+const showNotification = (toast) => {
+    toast('Hello');
+}
+
+showNotification();
+
+
